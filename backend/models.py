a065147c44bcb8b3d7bc2cd2181725b1eb0fd53f
JasmeetMudher <jsmudher4@gmail.com>
2026-03-18 21:38:56 +0300

    Web component Refactor

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ import uuid
 
 class Todo(SQLModel, table=True):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
-    user_id: Optional[str] = None 
+    user_id: Optional[str] = None
     task: str
     priority: str
     task_type: str
@@ -17,6 +17,6 @@ class Todo(SQLModel, table=True):
 class User(SQLModel, table=True):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     username: str
-    password: str  
+    password: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
